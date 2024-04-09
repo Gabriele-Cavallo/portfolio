ea0a936ed8d5629e1d91068ffa2b088bb2cef7de
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-04-09 22:11:33 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,154 @@
+const { createApp } = Vue;
+
+createApp({
+   data() {
+        return {
+            sections: [
+                {
+                    href: 'home',
+                    title: 'HOME',
+                },
+                {
+                    href: 'skills',
+                    title: 'SKILLS',
+                },
+                {
+                    href: 'my-repo',
+                    title: 'MY REPO',
+                },
+                {
+                    href: 'contact-me',
+                    title: 'CONTACT ME',
+                },
+            ],
+            skills: [
+                {
+                    instrument : 'HTML',
+                    status: true,
+                    logo: './img/html-5.png',
+                    learned: 'rgb(3, 255, 3)',
+                    inProgress: 'red',
+                    description: 'Medium-level basic knowledge of the instrument and independent research ability for the expansion of skills.',
+                    competents: [ 'prima','seconda','terza','quarta','quinta'],
+                },
+                {
+                    instrument : 'CSS',
+                    status: true,
+                    logo: './img/css-logo.png',
+                    learned: 'rgb(3, 255, 3)',
+                    inProgress: 'red',
+                    description: 'Medium-level basic knowledge of the instrument and independent research ability for the expansion of skills.',
+                    competents: [ 'prima','seconda','terza','quarta','quinta'],
+                },
+                {
+                    instrument : 'BOOTSTRAP',
+                    status: true,
+                    logo: './img/bootstrap-logo.png',
+                    learned: 'rgb(3, 255, 3)',
+                    inProgress: 'red',
+                    description: 'Medium-level basic knowledge of the instrument and independent research ability for the expansion of skills.',
+                    competents: [ 'prima','seconda','terza','quarta','quinta'],
+                },
+                {
+                    instrument : 'JS',
+                    status: true,
+                    logo: './img/js-logo.png',
+                    learned: 'rgb(3, 255, 3)',
+                    inProgress: 'red',
+                    description: 'Medium-level basic knowledge of the instrument and independent research ability for the expansion of skills.',
+                    competents: [ 'prima','seconda','terza','quarta','quinta'],
+                },
+                {
+                    instrument : 'VUE-JS',
+                    status: true,
+                    logo: './img/vue-logo.png',
+                    learned: 'rgb(3, 255, 3)',
+                    inProgress: 'red',
+                    description: 'Medium-level basic knowledge of the instrument and independent research ability for the expansion of skills.',
+                    competents: [ 'prima','seconda','terza','quarta','quinta'],
+                },
+                {
+                    instrument : 'PYTHON',
+                    status: false,
+                    logo: './img/python-logo.png',
+                    learned: 'rgb(3, 255, 3)',
+                    inProgress: 'red',
+                    description: 'Basic-level knowledge of the instrument and independent research ability for the expansion of skills.',
+                    competents: [ 'prima','seconda','terza','quarta','quinta'],
+                },
+            ],
+            myRepo: [
+                {
+                    projects: 'Boolzapp',
+                    image: 'random-img',
+                    link: 'https://github.com/Gabriele-Cavallo/vue-boolzapp'
+                },
+                {
+                    projects: 'Campo minato vue',
+                    image: 'random-img',
+                    link: 'https://github.com/Gabriele-Cavallo/campo-minato-vue'
+                },
+                {
+                    projects: 'Vue Slider',
+                    image: 'random-img',
+                    link: 'https://github.com/Gabriele-Cavallo/vue-slider'
+                },
+                {
+                    projects: 'Vue Todo List',
+                    image: 'random-img',
+                    link: 'https://github.com/Gabriele-Cavallo/vue-todolist'
+                },
+                {
+                    projects: 'Vue Slider',
+                    image: 'random-img',
+                    link: 'https://github.com/Gabriele-Cavallo/vue-slider'
+                },
+                {
+                    projects: 'Vue Todo List',
+                    image: 'random-img',
+                    link: 'https://github.com/Gabriele-Cavallo/vue-todolist'
+                },
+                {
+                    projects: 'Boolzapp',
+                    image: 'random-img',
+                    link: 'https://github.com/Gabriele-Cavallo/vue-boolzapp'
+                },
+                {
+                    projects: 'Campo minato vue',
+                    image: 'random-img',
+                    link: 'https://github.com/Gabriele-Cavallo/campo-minato-vue'
+                },
+                {
+                    projects: 'Vue Slider',
+                    image: 'random-img',
+                    link: 'https://github.com/Gabriele-Cavallo/vue-slider'
+                },
+                {
+                    projects: 'Vue Todo List',
+                    image: 'random-img',
+                    link: 'https://github.com/Gabriele-Cavallo/vue-todolist'
+                },
+                {
+                    projects: 'Vue Slider',
+                    image: 'random-img',
+                    link: 'https://github.com/Gabriele-Cavallo/vue-slider'
+                },
+                {
+                    projects: 'Vue Todo List',
+                    image: 'random-img',
+                    link: 'https://github.com/Gabriele-Cavallo/vue-todolist'
+                },
+            ],
+            activeSection: 0,
+            
+        };
+    },
+    methods: {
+        selectSection(index){
+            this.activeSection = index;
+        }
+
+    },
+    mounted() {
+    }
+}).mount('#app');
---
+++ readme.md
@@ -0,0 +1,10 @@
+SECTIONS:
+
+    Per aggiungere una sezione alla pagina basta usare il Template single section
+    commentato in fondo a tutte le section nell'HTML e poi aggiungere l'oggetto con le chiavi
+    href e title nell'array sections di js.
+
+    ---ATTENZIONE---
+    la chiave HREF valorizza il link di href nel menù nav-bar e
+    nel template HMTL, con l'aggiunta del nuovo indice, regola anche il valore dell'ID 
+    da collegare all'href del menu nav-bar
